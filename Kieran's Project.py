4f3953a0588f843ef231ae2273456a21313ff0bd
kieranmoran <kieranpmoran@gmail.com>
2021-06-25 18:45:46 +0100

    UCD Project (updated)

--- Kieran's Project.py
+++ Kieran's Project.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 data = pd.read_csv('gdp_csv.csv')
 
+
 #make a copy of the data
 df = data.copy()
 
@@ -15,9 +16,21 @@ print(df.tail())
 #checking if there are some missing values
 print(df.isnull().sum())
 
-df["Country Name"].unique()
+#Dataframe -Stocks
+stocks = pd.read_csv("NSE_BANKING_SECTOR.csv")
 
-df.types 
+print(stocks.info)
+print(stocks.describe())
+print(stocks.dropna())
+
+#sorting dataframe - you pass a single argument to the method containing the name of the column you want to sort by.
+print(stocks.sort_values("TURNOVER"))
+print(stocks.head(20))
+
+
+
+
+
 
 
 
