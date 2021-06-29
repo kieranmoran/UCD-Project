d842ad5b4355d870c85d839212b483b1bced3c02
kieranmoran <kieranpmoran@gmail.com>
2021-06-29 21:36:07 +0100

    UCD Project - Additional Steps required for project

--- Kieran's Project.py
+++ Kieran's Project.py
@@ -37,17 +37,42 @@ print(df.head())
 #Creating a list in python
 
 job = ["admin", "technican", "management", "blue_collar", "services", "student", "self_employed", "housemaid", "retired", "un_employed", "entrepreneur"]
-
-
 Martial_Status = ["single", "married", "divorced"]
+
+
+
 #I want to subset the jobs were people arent working
 jobs_subset = job[8:10]
 print(jobs_subset)
 
-max_age =
 
 
-Bank_Customers = pd.read_csv("")
+
+#New Dataset
+NSE_Banking_Sector = pd.read_csv("NSE_BANKING_SECTOR.csv")
+print(NSE_Banking_Sector)
+
+import matplotlib.pyplot as plt
+fig,ax = plt.subplots()
+plt.show()
+
+x = NSE_Banking_Sector['volume'].head(5)
+y1 = NSE_Banking_Sector['turnover'].head(5)
+y2 = NSE_Banking_Sector['symbol'].head(5)
+
+ax.plot(x,y1)
+ax.plot(x,y2)
+
+ax.plot(x,y1, marker="v", linestyle="--", color="r")
+import matplotlib.pyplot as plt
+import seaborn as sns
+fig,ax = plt.subplots()
+plt.show()
+
+seaborn
+sns.lineplot(x=data['video_id'].head(25),
+		y=data['views'].head(25))
+
 
 
 
