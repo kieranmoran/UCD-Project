219c139f8bf989f9e6e3ee5d61bd97ce5114bb54
kieranmoran <kieranpmoran@gmail.com>
2021-06-29 21:47:12 +0100

    UCD Project - Additional Steps required for project

--- Kieran's Project.py
+++ Kieran's Project.py
@@ -33,6 +33,52 @@ print(stocks.head(20))
 Bank_Customers = pd.read_csv("data.csv")
 
 print(df.head())
+#Count missing values in each column
+missing_values_count = netflix_data.isnull().sum()
+print(missing_values_count[0:5])
+
+#Drop rows where data is missing
+droprows= netflix_data.dropna()
+print(netflix_data.shape,droprows.shape)
+#Drop Colunm where data is missing
+dropcolumns = netflix_data.dropna(axis=1)
+print(netflix_data.shape,dropcolumns.shape)
+
+#Fill All missing values with 0
+cleaned_data = netflix_data.fillna(0)
+#Fill all missing values to the value that comes next in the same column
+cleaned_data = netflix_data.fillna(method='bfill', axis=0).fillna(0)
+
+#Drop rows where data is missing
+droprows= netflix_data.dropna()
+print(netflix_data.shape,droprows.shape)
+
+#Drop Columns where data is missing
+dropcolumns = netflix_data.dropna(axis=1)
+print(netflix_data.shape,dropcolumns.shape)
+#Fill all missing values with 0
+cleaned_data = netflix_data.fillna(0)
+
+#Fill all missing values to the value that comes next in the same column
+cleaned_data = netflix_data.fillna(method='bfill', axis=0).fillna(0)
+#Fill all missing values to the value that comes next in the same column
+cleaned_data = netflix_data.fillna(method='bfill', axis=0).fillna(0)
+
+#Drop all rows that are duplicates
+drop_duplicates= netflix_data.drop_duplicates()
+print(netflix_data.shape,drop_duplicates.shape)
+#Drop Duplicate Rows based on specific columns
+drop_duplicates= netflix_data.drop_duplicates(subset=['show_id'])
+print(netflix_data.shape,drop_duplicates.shape)
+
+
+
+
+
+
+
+
+
 
 #Creating a list in python
 
